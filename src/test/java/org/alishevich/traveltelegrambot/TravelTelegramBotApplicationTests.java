824394bdb25d5cca8723c13f36124556df756ab6
Ekaterina Alishevich <ekaterina.alishevich@gmail.com>
2021-05-15 15:28:04 +0300

    added SpringBot skeleton

---
+++ src/test/java/org/alishevich/traveltelegrambot/TravelTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alishevich.traveltelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
